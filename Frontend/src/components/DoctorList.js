8becb0bb510c294f7b75e80cd44bbe71c6be3788
shrihari143 <126378618+shrihari143@users.noreply.github.com>
2023-09-18 09:57:15 +0530

    Complete_Code

---
+++ Frontend/src/components/DoctorList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import"../styles/dl.css";
+const DoctorList = ({ doctor }) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div
+        className="card m-2"
+        
+      >
+        <div className="card-header">
+          Dr. {doctor.firstName} {doctor.lastName}
+        </div>
+        <div className="card-body">
+          <p>
+            <b>Specialization</b> {doctor.specialization}
+          </p>
+          <p>
+            <b>Highest Qualification</b> {doctor.website}
+          </p>
+          <p>
+            <b>Experience</b> {doctor.experience}
+          </p>
+          <p>
+            <b>Fees Per Cunsaltation</b> {doctor.feesPerCunsaltation}
+          </p>
+          
+          <p>
+            <b>Timings</b> {doctor.timings[0]} - {doctor.timings[1]}
+          </p>
+          <button className="doctorlist"style={{ cursor: "pointer" }}
+        onClick={() => navigate(`/doctor/book-appointment/${doctor._id}`)}>Apply</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DoctorList;
